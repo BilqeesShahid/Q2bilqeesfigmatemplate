0f5bab91085ee801560d2613f62a9818b919f53f
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-10-15 20:33:33 +0500

    Update BrowseProducts.tsx

--- src/components/BrowseProducts.tsx
+++ src/components/BrowseProducts.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const BrowseProducts = () => {
   return (
 
-    <div className='mb-[100px] mt-[100px] bg-slate-300'>
+     <div className='mb-[100px] mt-[100px] bg-slate-300'>
       {/* Heading */}
       <div className="text-center mb-10">
         <h1 className="text-myblue scroll-m-20 text-2xl font-bold tracking-tight lg:text-3xl">
@@ -16,25 +16,25 @@ const BrowseProducts = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center items-center">
   <div className="max-w-[350px] h-[25rem] p-2 shadow-md rounded-xl relative group">
     <div>
-      <Image src="/sofa5.jpg" alt="sofa" width={400} height={400} className="w-full h-full object-cover" />
+      <Image src="/sofa2.jpg" alt="sofa" width={400} height={400} className="w-full h-full object-cover" />
     </div>
     <h2 className="card-title text-2xl font-bold text-myblue text-center mt-2">Smart Sofa</h2>
   </div> 
   <div className="max-w-[350px] h-[25rem] p-2 shadow-md rounded-xl relative group">
     <div>
-      <Image src="/table.png" alt="table" width={400} height={400} className="w-full h-full object-cover" />
+      <Image src="/divider.webp" alt="table" width={400} height={400} className="w-full h-full object-cover" />
     </div>
     <h2 className="card-title text-2xl font-bold text-myblue text-center mt-2">Smart Table</h2>
   </div> 
   <div className="max-w-[350px] h-[25rem] p-2 shadow-md rounded-xl relative group">
     <div>
-      <Image src="/chair3.webp" alt="chair" width={400} height={400} className="w-full h-full object-cover" />
+      <Image src="/sofa12.jpg" alt="chair" width={400} height={400} className="w-full h-full object-cover" />
     </div>
     <h2 className="card-title text-2xl font-bold text-myblue text-center mt-2">Smart Chair</h2>
   </div> 
 </div>
 </div>
-  )
+    )
 }
 
 export default BrowseProducts
