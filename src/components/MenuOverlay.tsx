02a0d21c3354c3a9e17d50f7bf236e3be43389aa
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-10-15 20:11:24 +0500

    Update MenuOverlay.tsx

--- src/components/MenuOverlay.tsx
+++ src/components/MenuOverlay.tsx
@@ -9,7 +9,7 @@ interface PropsType{
 
 const MenuOverlay : React.FC<PropsType> = ({links}) => {
   return (
-    <ul className='flex flex-col py-4 items-center'>{links.map((link: { path: string; title: string; },index: any) => 
+    <ul className='flex flex-col py-4 items-center'>{links.map((link: { path: string; title: string; },index: string) => 
       (
         <li key={index}>
  <NavLink href={link.path} title={link.title}/>
